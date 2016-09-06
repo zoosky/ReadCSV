3884c1ed819dc01f57fff015d4b4c170eb9a491c
zoosky <zoosky@gmail.com>
2016-09-06 08:35:07 +0200

    more instructions

--- ReadCSV.scala
+++ ReadCSV.scala
@@ -4,7 +4,7 @@ import java.io._
 
 object ReadCSV {
   def main(args: Array[String])  {
-    println("Hi! Put your 'sample.csv' in the start directory. I read from there.")
+    println("Hi! Put your 'sample.csv' in the start directory. I read it from there.\nUse vi -b (binary mode to edit. Values should be separated with ';'.")
 
     val f = CsvParser[String,String].parse(new java.io.FileReader("sample.csv"), ";")
     println(f)
